d5237979c79931a7accdeda950605430a13318a4
HARSH TIWARI <ht728350@gmail.com>
2026-03-19 22:22:13 +0530

    update in code

--- backend_app/app/User_Api/fetch_order.py
+++ backend_app/app/User_Api/fetch_order.py
@@ -1,3 +1,12 @@
 from flask_smorest import Blueprint
 from flask.views import MethodView
 from ..authorise import get_current_user
+from ..Database.User.check_out import OrderTshirt
+
+blp = Blueprint('user seen all order', __name__)
+
+
+@blp.route('user/fetch/order')
+class UserFetchOrder(MethodView):
+    def __init__(self):
+        self.order_db = OrderTshirt()
