9f6d330c4175fc79824c0bbc70eb166163ec220b
Harsh Tiwari <info.madhut@gmail.com>
2026-04-23 19:23:17 +0530

    update in backend code

--- backend_app/run.py
+++ backend_app/run.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from app import create_app
 from dotenv import load_dotenv
 from app.extensions import socketio
@@ -6,9 +9,5 @@ load_dotenv()
 app = create_app()
 socketio.init_app(app)
 
-
-
 if __name__ == "__main__":
     socketio.run(app)
-
-
